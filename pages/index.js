794db7edcdcf1de731c2cb68de8a9fef2017a7f4
Victor Hugo Duarte <victorduarte@sisconsultoria.com.br>
2022-07-04 12:52:11 -0400

    Cria modal para softwareHouse

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,13 @@ export default function Home() {
   const handleClose = () =>{
     setOpen(false)
   }
+  const xxx =(eee) =>{console.log(eee)}
+  
 
   return (
     <>
         {/*Necessário se não bugava a página */}
-        {open && ( <Modal open={open} handleClose={handleClose}/>)}
+        {open && ( <Modal open={open} handleClose={handleClose}setData={xxx}/> )}
        
         <Button variant="contained" color="primary" onClick={()=>{
           setOpen(true)
