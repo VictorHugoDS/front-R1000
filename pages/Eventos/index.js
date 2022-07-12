48b5e1334d67d26b643cbe0d093df74d930c24f0
matheus <mat.feijo2506@gmail.com>
2022-07-12 13:28:22 -0400

    feat integracao eventos com back

--- pages/Eventos/index.js
+++ pages/Eventos/index.js
@@ -1,36 +1,43 @@
 import moment from "moment"
-import Layout from "../../components/layout";
-import { withStyles, Button } from "@material-ui/core"
+import { Modal, CircularProgress } from "@material-ui/core"
 import { useEffect, useState } from "react";
-import { GetAll } from "../../lib/api";
-
-export const ButtonCSS = withStyles({
-    root: {
-        backgroundColor: ""
-    }
-
-})(Button);
-
+import { api, GetAll } from "../../lib/api";
+import Home from "../efd/editar/[id]";
 
 export default function Eventos() {
 
-    const [data,setData] = useState();
-    const [cardss,setCards] = useState()
+    const [data, setData] = useState();
+    const [cardss, setCards] = useState();
+    const [openModal, setOpenModal] = useState(false);
+    const [detailsCard, setDetailsCard] = useState();
+    const [openModalDelete, setOpenModalDelete] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [openEditEvent, setOpenEditEvent] = useState(false);
 
-    useEffect(()=>async()=>{
-        await GetAll('/contribuinte',setData)
-    },[])
+    useEffect(() => async () => {
+        setLoading(true);
+        await GetAll('/contribuinte/', setData).then((res) => console.log(res)).catch((err) => console.log(err))
+        setLoading(false);
+    }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         const vetor = []
-        if(data){
-            data.forEach((card, ind)=>{
-                
+        if (data) {
+            data.forEach((card, ind) => {
+                vetor.push({
+                    data_inicio: new Date(card.infoContri.inclusao.inivalid),
+                    data_fim: new Date(card.infoContri.inclusao.fimValid),
+                    Recibo: card.ideContri.nrInsc,
+                    id: card.id,
+                    ...card
+                })
             })
+
+            setCards(data);
         }
-    },[data])
+    }, [data])
 
-
+    /**SO EXCLUIR ESSE ARRAY E TROCAR O NOME DO CARDS PARA CARDSS.MAP */
     const cards = [
         {
             id: 0,
@@ -39,40 +46,47 @@ export default function Eventos() {
             data_fim: new Date,
         },
         {
-            id: 1,
+            id: 0,
             Recibo: 'Numero do recibo',
             data_inicio: new Date(),
             data_fim: new Date,
         },
         {
-            id: 2,
+            id: 0,
             Recibo: 'Numero do recibo',
             data_inicio: new Date(),
             data_fim: new Date,
         },
-        {
-            id: 3,
-            Recibo: 'Numero do recibo',
-            data_inicio: new Date(),
-            data_fim: new Date,
-        }
+
     ]
 
 
-    const handleDelete = (e, idCard) => {
-        e.preventDefault();
-        cards.forEach((card, ind) => {
-            if (card.id === idCard) {
-                cards.splice(1, ind);
-            }
-        })
+    const handleDelete = (e, card) => {
+        setDetailsCard(card);
+        setOpenModalDelete(true);
+        //setOpenModalDelete(true);
+
+    }
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    }
+
+    const handleOpenModal = () => {
+        setOpenModal(true);
+    }
+
+    const handleSubmitDelete = async () => {
+        setLoading(true);
+        await api.delete(`/contribuinte/${detailsCard.id}`).then((res) => console.log(res)).catch((err) => console.log(err))
+        setLoading(false);
     }
 
     return (
         <div className="container-listar-eventos">
             <h1 className="titulo-principal">EFD REINF</h1>
 
-            <div className="content-eventos">
+            {loading ? <CircularProgress style={{ position: "absolute", left: "50%", top: "50%" }} /> : <div className="content-eventos">
                 <h1 className="titulo-eventos">Eventos Enviados</h1>
 
                 {cards.map((card, index) => {
@@ -81,7 +95,11 @@ export default function Eventos() {
                             <div className="container-num-recibo">
 
                                 <p>Recibo:{card.Recibo}</p>
-                                <a href="#">ver</a>
+                                <a href="" onClick={(e) => {
+                                    e.preventDefault();
+                                    handleOpenModal();
+                                    setDetailsCard(card);
+                                }}>ver</a>
 
                             </div>
 
@@ -95,8 +113,11 @@ export default function Eventos() {
                                     <p>Fim:{moment(card.data_fim).format('L')}</p>
                                 </div>
                                 <div>
-                                    <button className="button-edit">Editar</button>
-                                    <button className="button-excluir" onClick={(e) => handleDelete(e, card.id)}>Excluir</button>
+                                    <button className="button-edit" onClick={(e) => {
+                                        e.preventDefault();
+                                        setOpenEditEvent(true);
+                                    }}>Editar</button>
+                                    <button className="button-excluir" onClick={(e) => handleDelete(e, card)}>Excluir</button>
                                 </div>
                             </div>
                         </div>
@@ -114,7 +135,126 @@ export default function Eventos() {
                         Criar evento
                     </button>
                 </div>
-            </div>
-        </div>
+            </div>}
+
+            {
+                openEditEvent && <Home />
+            }
+
+            {openModalDelete &&
+                <Modal
+                    open={openModalDelete}
+                    onClose={(e) => {
+                        setOpenModalDelete(false);
+                    }}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <div style={{
+                        minWidth: "40vh",
+                        minHeight: "10vh",
+                        backgroundColor: "#ffff",
+                        borderRadius: "4px",
+                        padding: "10px 20px",
+                        textAlign: "center",
+                    }}>
+
+                        <p style={{ fontWeight: 700 }}>Tem certeza que deseja excluir evento?</p>
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "space-around",
+
+                        }}>
+                            <button
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    setOpenModalDelete(false);
+                                }}
+                                style={{
+                                    border: 'none',
+                                    backgroundColor: "#EF0A0A",
+                                    color: "#fff",
+                                    cursor: "pointer",
+                                    width: "50px",
+                                    height: "25px",
+                                    borderRadius: "4px",
+                                }}>Não
+                            </button>
+                            <button
+                                onClick={handleSubmitDelete}
+                                style={{
+                                    border: 'none',
+                                    backgroundColor: "#3290FF",
+                                    color: "#fff",
+                                    cursor: "pointer",
+                                    width: "50px",
+                                    height: "25px",
+                                    borderRadius: "4px",
+                                }}>Sim</button>
+                        </div>
+                    </div>
+                </Modal>
+            }
+            {
+                openModal &&
+                <Modal open={openModal}
+                    onClose={handleCloseModal}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <div style={{
+                        minWidth: "40vh",
+                        minHeight: "30vh",
+                        backgroundColor: "#ffff",
+                        borderRadius: "4px",
+                        padding: "10px 20px",
+                    }}>
+
+                        <div>
+                            <p>{detailsCard.id}</p>
+                            <p>{detailsCard.ideEvento.tpAmb}</p>
+                            <p>{detailsCard.ideEvento.procEmi}</p>
+                            <p>{detailsCard.ideEvento.verProc}</p>
+                            <p>{detailsCard.ideContri.tpInsc}</p>
+                            <p>{detailsCard.ideContri.nrInsc}</p>
+
+                            <p>{detailsCard.infoContri.inclusao.idePeriodo.iniValid}</p>
+                            <p>{detailsCard.infoContri.inclusao.idePeriodo.fimValid}</p>
+
+                            <div style={{ paddingLeft: "10px" }}>
+                                <p style={{ fontWeight: 700 }}>Informações do cadastro</p>
+                                <ul>
+                                    <li>{detailsCard.infoContri.infoCadastro.clasTrib}</li>
+                                    <li>{detailsCard.infoContri.infoCadastro.indEscrituracao}</li>
+                                    <li>{detailsCard.infoContri.infoCadastro.indDesoneracao}</li>
+                                    <li>{detailsCard.infoContri.infoCadastro.indAcordoSemMulta}</li>
+                                    <li>{detailsCard.infoContri.infoCadastro.indSitPJ}</li>
+                                    <li>{detailsCard.infoContri.infoCadastro.indUniao}</li>
+                                    <li>{detailsCard.infoContri.infoCadastro.dtTransfFinsLucr}</li>
+                                    <li>{detailsCard.infoContri.infoCadastro.dtObito}</li>
+                                </ul>
+
+                                <div style={{ paddingLeft: "10px" }}>
+                                    <p style={{ fontWeight: 700 }}>Informações de contato</p>
+                                    <ul>
+                                        <li>{detailsCard.infoContri.infoCadastro.contato.nmCtt}</li>
+                                        <li>{detailsCard.infoContri.infoCadastro.contato.cpfCtt}</li>
+                                        <li>{detailsCard.infoContri.infoCadastro.contato.foneFixo}</li>
+                                        <li>{detailsCard.infoContri.infoCadastro.contato.foneCel}</li>
+                                        <li>{detailsCard.infoContri.infoCadastro.contato.email}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Modal>
+            }
+        </div >
     )
 }
